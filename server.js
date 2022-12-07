2a3cf98dcff93fa80ba93e8c9d5fc3cbd36db742
Kiran Adhikari <kiranadh1452@gmail.com>
2022-12-08 00:14:25 +0545

    Server root file + Swagger docs

---
+++ server.js
@@ -0,0 +1,45 @@
+/*----------------------
+Dependencies and Imports
+------------------------*/
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// routers
+
+// constants
+const PORT = require("./constants/appConfig").PORT;
+
+// swagger for documentation
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
+// configure application
+dotenv.config();
+const app = express();
+app.set("port", PORT);
+// use morgan logger here
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// add routes to server
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// for any endpoint that is not found, follow this route
+app.use("*", (req, res) => {
+    console.error(`Endpoint not found: ${req.originalUrl}`);
+    return res.status(400).json({
+        message: "Endpoint not found",
+    });
+});
+
+// start server
+app.listen(PORT, (err) => {
+    if (err) {
+        logger.error("there was a problem", err);
+        return;
+    }
+    console.log(`server listening on ${PORT}`);
+});
